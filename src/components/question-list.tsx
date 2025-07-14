95ef4b55f645a11a869c9c7c39745267ca6f4a0b
Gabrielle Adriano <gabrielle.a.adriano@gmail.com>
2025-07-14 17:55:14 -0300

    feat: create ui

---
+++ src/components/question-list.tsx
@@ -0,0 +1,17 @@
+import { QuestionItem } from './question-item'
+
+interface QuestionListProps {
+  roomId: string
+}
+
+export function QuestionList(props: QuestionListProps) {
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <h2 className="font-semibold text-2xl text-foreground">
+          Perguntas & Respostas
+        </h2>
+      </div>
+    </div>
+  )
+}
